d81c20e0fe1583721436dcf9d353885942f0c637
rajkumar <rajkumar.gootam@gmail.com>
2016-02-02 17:25:41 +0530

    if i click on the first cell it will print cross(X) and if i click on another cells it wii change to Zero(0)

--- js/game.js
+++ js/game.js
@@ -1,6 +1,16 @@
 $(document).ready(function (){
+  var isCrossSelected = 0;
   $(".cells").click(function () {
       var cells = $(this);
-      cells.addClass('cross');
+      if (isCrossSelected == 0 ){
+        isCrossSelected = 1;
+        cells.addClass('cross');
+      }else{
+        isCrossSelected = 0;
+        cells.addClass('zero');
+      }
+      // isCrossSelected=!isCrossSelected;
+
+      // cells.addClass('cross').addClass('zero');
   })
 });
